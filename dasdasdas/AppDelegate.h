253f74fb71af43e62c031b0167ce75f2632aee36
泽秦严 <zeqinyan@zeqindeMacBook-Pro.local>
2017-06-27 15:24:22 +0800

    Initial Commit

---
+++ dasdasdas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dasdasdas
+//
+//  Created by 泽秦  严 on 2017/6/27.
+//  Copyright © 2017年 泽秦  严. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dasdasdas/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dasdasdas
+//
+//  Created by 泽秦  严 on 2017/6/27.
+//  Copyright © 2017年 泽秦  严. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
